7e52e681594600c93c6f95eacd89b15eb825fc6f
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2023-02-25 08:21:24 +0000

    Flashメッセージの設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,8 +15,9 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to article_path(@article)
+      redirect_to article_path(@article), notice: 'Saved!'
     else
+      flash.now[:error] = 'Save failed....'
       render :new
     end
   end
