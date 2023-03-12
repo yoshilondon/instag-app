92481710589c391958924b298c07c4f05f0bdc8c
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2023-03-12 20:30:20 +0000

    articleのindex.htmlとnew.htmlのcss調整。

---
+++ app/helpers/time_helper.rb
@@ -0,0 +1,7 @@
+module TimeHelper
+  def post_time(datetime)
+    time_ago_in_words(datetime)
+  end
+end
+
+
--- app/models/article.rb
+++ app/models/article.rb
@@ -18,4 +18,8 @@ class Article < ApplicationRecord
   validates :content, presence: true
 
   belongs_to :user
+
+  def author_name
+    user.display_name
+  end
 end
