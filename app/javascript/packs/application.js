5a581cf7f8e9e3392fb119ba1e112759f69c9fbd
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2023-04-15 13:54:22 +0100

    いいね機能のJSでの実装完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import $ from 'jquery'
 import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import axios from 'axios'
-import { csrfToken } from 'rails-ujs'
-
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 Rails.start()
 // Turbolinks.start()
@@ -21,77 +16,9 @@ ActiveStorage.start()
 //= require popper
 //= require bootstrap
 
-document.addEventListener('DOMContentLoaded', () => {
-  const update = document.getElementById('profile_avatar')
-  if (update) {
-    update.addEventListener('change', (e) => {
-      const file = update.files[0];   
-      const reader = new FileReader(); 
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        const image = reader.result;
-        console.log(image);
-        document.querySelector('.avatar-image').setAttribute('src', image);
-      }
-      $('.submit-btn').removeClass('hidden')
 
-    });
-  }
-});
 
-// articles/new 投稿ボタン（Post）の設定
-document.addEventListener('DOMContentLoaded', () => {
-  $("#postBtn").on('click', () => {
-    $('form').submit();
-  });
-});
 
-// Likeのハートの表示切替
-const handleHeartDisplay = (hasLiked) => {
-  if (hasLiked) {
-    $('.active-heart').removeClass('hidden')
-  } else {
-    $('.inactive-heart').removeClass('hidden')
-  }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const dataset = $('#article-show').data()
-  const articleId = dataset.articleId
-  axios.get(`/articles/${articleId}/like`)
-    .then((response) => {
-      const hasLiked = response.data.hasLiked
-      handleHeartDisplay(hasLiked)
-    })
-
-  $('.inactive-heart').on('click', () => {
-    axios.post(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === 'OK') {
-          $('.active-heart').removeClass('hidden');
-          $('.inactive-heart').addClass('hidden');
-        }
-      })
-      .catch((e) => {
-        window.alert('Error')
-        console.log(e)
-      })
-  })
-
-  $('.active-heart').on('click', () => {
-    axios.delete(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === 'OK') {
-          $('.active-heart').addClass('hidden')
-          $('.inactive-heart').removeClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('Error')
-        console.log(e)
-      })
-  })
-})
 
 // $('.profilePage_image').on('click', () => {
 //   axios.get('/profile')
