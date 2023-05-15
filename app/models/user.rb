6c4f53af0fd7e7c8a50f879d1957d0bd377454a2
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2023-05-15 21:50:29 +0100

    リレーションシップモデルの設定とフォローする関係構築

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,9 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
   has_one :profile, dependent: :destroy
 
   def has_liked?(article)
@@ -35,6 +38,10 @@ class User < ApplicationRecord
 
   def display_name
     profile&.accountname || self.email.split('@').first
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
   end
 
   def prepare_profile
