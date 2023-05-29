e400a24df7e61e5bd5a7d9dd42d60999cb647790
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2023-05-29 20:51:31 +0100

    フォロー記事一覧ページの作成・実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @articles = Article.all
