f57eb449cbcad10a141c1e425a635e2f27eae0f1
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2023-06-13 21:03:57 +0100

    moduleの実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
-  resource :timeline, only: [:show]
-  resources :timelines, only: [:index]
-
+  
   resources :articles
 
   resources :accounts, only: [:show] do
@@ -19,7 +17,11 @@ Rails.application.routes.draw do
     resources :followings, only: [:index]
   end
 
-  resource :profile, only: [:show, :edit, :update]
+  scope module: :apps do
+    resource :profile, only: [:show, :edit, :update]
+    resource :timeline, only: [:show]
+    resources :timelines, only: [:index]
+  end
 
   namespace :api, defaults: {format: :json} do
     scope '/articles/:article_id' do
